147cedb3111300e04a7f74d9e1da0c5a15ebcb24
leechan <dlcks0601@gmail.com>
2024-09-25 23:43:25 +0900

    9.25 프로그래머스 공부 기록

--- api-demo/express-demo.js
+++ api-demo/express-demo.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const app = express();
-app.listen(1234);
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.listen(process.env.PORT);
 
 app.get('/', function (req, res) {
-  res.send('Hello World');
+    res.send('Hello World');
 });
 
 let nodejsBook = {
-  title: 'Node.js를 공부해보자',
-  price: 20000,
-  description: '이 책 좋음',
+    title: 'Node.js를 공부해보자',
+    price: 20000,
+    description: '이 책 좋음',
 };
 
 app.get('/products/1', function (req, res) {
-  res.json(nodejsBook);
+    res.json(nodejsBook);
 });
---
+++ npm-demo/jwt-demo.js
@@ -0,0 +1,16 @@
+var jwt = require('jsonwebtoken');
+var dotenv = require('dotenv');
+
+dotenv.config();
+
+// 서명 = 토큰 발행
+var token = jwt.sign({ foo: 'bar' }, process.env.PRIVATE_KEY);
+// token 생성 = jwt 서명을 했따! (페이로드, 나만의 암호키) + SHA256
+
+console.log(token);
+
+// 검증
+// 만약 검증에 성공하면, 페이로드 값을 확인할 수 있음!
+
+var decoded = jwt.verify(token, process.env.PRIVATE_KEY);
+console.log(decoded);
